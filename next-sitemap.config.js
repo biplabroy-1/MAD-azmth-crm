205920b7011008f9ad0193385fb2efe738377c2f
Biplab Roy <biplabr119201@gmail.com>
2025-07-26 00:43:51 +0530

    fix: Update robots.txt generation setting to false

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://dashboard.azmth.in', // replace with your domain
-    generateRobotsTxt: true, // generates robots.txt file
+    generateRobotsTxt: false, // generates robots.txt file
     sitemapSize: 500,
     changefreq: 'daily',
     priority: 0.7,
