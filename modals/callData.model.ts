4286362be7b6428f2a02f19b97f2d7fa39b2cd2f
Biplab Roy <biplabr119201@gmail.com>
2025-07-31 11:49:27 +0530

    feat: Add indexes for improved query performance in CallData, CallQueue, and CallQueueDone schemas; remove unused favicon

--- modals/callData.model.ts
+++ modals/callData.model.ts
@@ -2,6 +2,11 @@ import mongoose, { Schema } from "mongoose";
 
 const CallDataSchema = new Schema({}, { strict: false });
 
+CallDataSchema.index({ userId: 1, startedAt: -1 });
+CallDataSchema.index({ "analysis.successEvaluation": 1 });
+CallDataSchema.index({ endedReason: 1 });
+CallDataSchema.index({ durationSeconds: 1 });
+
 // export default mongoose.model("CallData", CallDataSchema); 
 const CallData = mongoose.models.CallData || mongoose.model("CallData", CallDataSchema);
 export default CallData
